65fb6168398c3fdcecae27d51c65f7472c8a5e6e
chen <chencassc@gmail.com>
2024-03-12 11:30:28 +0800

    .

--- test/Motorbike.t.sol
+++ test/Motorbike.t.sol
@@ -4,6 +4,7 @@ import "forge-std/Test.sol";
 
 interface Motorbike{
     function upgrader() external view returns (address);
+    function horsePower() external view returns (uint256);
     function initialize() external;
     function upgradeToAndCall(address newImplementation, bytes memory data) external payable;
 }
@@ -30,9 +31,10 @@ contract PuzzleWalletTest is Test {
     }
 
     function test_motorbike() public {
-        Motorbike motorbike = Motorbike(0xFF0c4881ddFA0e8167B8533a3E10DA5AD4aeebc0);
         Attacker attacker =  new Attacker();
         attacker.attack();
+
+
         // This stops working from Cancun upgrade
         // vm.expectRevert();
         // motorbike.upgrader();
